4b5b3e19eca92263ceb36f37a4b4a27495d52b0f
LukeGabert <40614934+LukeGabert@users.noreply.github.com>
2022-03-29 14:22:11 -0400

    Add files via upload

---
+++ tradingStrategy.java
@@ -0,0 +1,18 @@
+
+public class tradingStrategy {
+
+	private int strategy;
+	
+	public tradingStrategy(int strategy) {
+		
+		this.strategy = strategy;
+		
+	}
+	
+	
+	public void setStrategy(int strategy) {
+		this.strategy = strategy;
+	}
+	
+	
+}
