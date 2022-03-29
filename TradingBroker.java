2f9a779896c12ad37bdbffc53c0bba26f266661b
eborg4 <102548467+eborg4@users.noreply.github.com>
2022-03-29 14:26:14 -0400

    Add files via upload

---
+++ TradingBroker.java
@@ -0,0 +1,14 @@
+package cryptoTrader.gui;
+
+public class TradingBroker {
+	private String tradingBroker;
+	
+	public TradingBroker(String tradingBroker) {
+		this.tradingBroker = tradingBroker;
+	}
+	
+	
+	public String tradingBroker() {
+		return tradingBroker;
+	}
+}
