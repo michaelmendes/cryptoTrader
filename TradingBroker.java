46ddd0ba9dda55657cb1c86d02cbdc81e472ea8a
eborg4 <102548467+eborg4@users.noreply.github.com>
2022-03-30 14:13:31 -0400

    Add files via upload

--- TradingBroker.java
+++ TradingBroker.java
@@ -2,13 +2,27 @@ package cryptoTrader.gui;
 
 public class TradingBroker {
 	private String tradingBroker;
+	private String[] coins;
+	private String strategy;
 	
-	public TradingBroker(String tradingBroker) {
+	public TradingBroker(String tradingBroker, String [] coins, String strategy) {
 		this.tradingBroker = tradingBroker;
+		this.coins = coins;
+		this.strategy = strategy;
 	}
 	
 	
-	public String tradingBroker() {
+	public String getTradingBroker() {
 		return tradingBroker;
+	}
+	
+	
+	public String[] getCoins() {
+		return coins;
+	}
+	
+	
+	public String getStrategy() {
+		return strategy;
 	}
 }
