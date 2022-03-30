0dbfaef59fcfcc18cd3d05d68c8b55271cc1eb77
LukeGabert <40614934+LukeGabert@users.noreply.github.com>
2022-03-30 14:14:46 -0400

    Add files via upload

---
+++ tradeActivity.java
@@ -0,0 +1,49 @@
+
+public class tradeActivity {
+
+	private String action, quantity, coin;
+	
+	public tradeActivity(String action, String quantity, String coin) {
+		
+		this.action = action;
+		this.quantity = quantity;
+		this.coin = coin;
+		
+	}
+	
+	
+	public String getAction() {
+		return action;
+	}
+
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public String getCoin() {
+		return coin;
+	}
+
+
+	public void setCoin(String coin) {
+		this.coin = coin;
+	}
+
+
+	public void getTradeData() {
+		
+	}
+	
+}
