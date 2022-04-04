60febe5e9a54eda36f262b9336df605ea31d307d
davidburnett93 <59963749+davidburnett93@users.noreply.github.com>
2022-04-04 15:04:33 -0400

    Add files via upload

--- StrategyA.java
+++ StrategyA.java
@@ -3,7 +3,6 @@ package cryptoTrader.utils;
 public class StrategyA implements TradingStrategies{
 	@Override
 	public String executeStrategy(String strategy, CryptoCoinList list) {
-		System.out.println("We are here");
 		if(list.searchCryptoCoinList("BTC") && list.searchCryptoCoinList("ETH") && list.getCryptoCoinList().length == 2) {
 			CryptoCoin bitcoin = list.getCoin("BTC");
 			double price = bitcoin.getCoinPrice();
